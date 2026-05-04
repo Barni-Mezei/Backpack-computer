77425ba5e249867f04f4f5732dd4472b0044a2a8
Barni <mezeibg@gmail.com>
2026-05-05 00:01:43 +0200

    Replaced the model with the original from scrap mechanic

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ import * as THREE from 'three';
 
 import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
 import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
+import { ColladaLoader } from 'three/addons/loaders/ColladaLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-let camera, renderer, controls, object;
+let camera, renderer, controls, model;
 
 let scene = new THREE.Scene();
 let light;
@@ -12,11 +13,20 @@ let light;
 // Create screen canvas
 let c = document.getElementById("maincv");
 let ctx = c.getContext("2d");
-let ct; // Canvas texture
 
-let margin = 25;
-c.width = 256 + margin*2;
-c.height = 256 + margin*2;
+c.width = 1024;
+c.height = 1024;
+
+// Create the texture from the canvas
+let ct = new THREE.CanvasTexture(c);
+ct.minFilter = THREE.NearestFilter;
+ct.magFilter = THREE.NearestFilter;
+ct.wrapS = THREE.RepeatWrapping;
+ct.wrapT = THREE.RepeatWrapping;
+ct.center.set(0.5, 0.5);
+ct.repeat.set(1.0, 1.0);
+
+let image, mask;
 
 /********/
 /* MAIN */
@@ -25,54 +35,59 @@ c.height = 256 + margin*2;
 init();
 
 async function init() {
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 40);
-    camera.position.z = 1.25;
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 20);
+    camera.position.z = 5;
 
-    const ambientLight = new THREE.AmbientLight(0xffeedd);
+    const ambientLight = new THREE.AmbientLight(0xffeedd, 0.75);
     scene.add(ambientLight);
 
-    light = new THREE.PointLight(0xffffff, 15);
+    light = new THREE.PointLight(0xffeedd, 15);
     camera.add(light);
     scene.add(camera);
 
-    // Load the textures
-    const mtlLoader = new MTLLoader().setPath('res/models/');
-    const materials = await mtlLoader.loadAsync('computer_backpack.mtl');
+    scene.background = new THREE.Color().setHex(0x0f160c);
+
+    // Load the materials (later they will be overwritten later)
+    const mtlLoader = new MTLLoader().setPath("res/models/sm/");
+    const materials = await mtlLoader.loadAsync("backpack_computer.mtl");
     materials.preload();
 
-    // Find the screen texture and replace it with the canvas texture
-    let screenUUID = "";
+    // Load color textures
+    let iLoader = new THREE.ImageLoader();
+    image = await iLoader.loadAsync("res/models/sm/backpack_computer_diff.png");
+    image.colorSpace = THREE.SRGBColorSpace;
+    
+    // Load model textures
+    let tLoader = new THREE.TextureLoader();
+    let textureNormal = tLoader.load("res/models/sm/backpack_computer_nor.png");
+    textureNormal.colorSpace = THREE.SRGBColorSpace;
+    let textureAsg = tLoader.load("res/models/sm/backpack_computer_asg.png");
+    textureAsg.colorSpace = THREE.SRGBColorSpace;
+    let textureMask = tLoader.load("res/models/sm/backpack_computer_mask.png");
+    textureMask.colorSpace = THREE.SRGBColorSpace;
 
-    for (let uuid in materials.materialsInfo) {
-        if (materials.materialsInfo[uuid].map_kd == "screen.png") {
-            screenUUID = uuid;
-        }
-    }
+    let material = new THREE.MeshStandardMaterial({
+        color: 0xaaaaaa, // Darken the model
+        map: ct,
+        normalMap: textureNormal,
+        aoMap: textureAsg,
 
-    // Replace material, if it was found
-    if (screenUUID != "") {
-        // Create the texture from the canvas
-        ct = new THREE.CanvasTexture(c);
-        ct.minFilter = THREE.NearestFilter;
-        ct.magFilter = THREE.NearestFilter;
-        ct.wrapS = THREE.RepeatWrapping;
-        ct.wrapT = THREE.RepeatWrapping;
-        ct.center.set(0.5, 0.5);
-        ct.repeat.set(1.0, 1.0);
+        roughness: 1.0,
+        metalness: 0.0
+    });
 
-        // Assign the new screen material
-        materials.materials[screenUUID] = new THREE.MeshStandardMaterial({ map: ct, roughness: 1.0, metalness: 0.0 });
-    }
+    materials.materials.m_main = material;
 
     // Load the model
-    const objLoader = new OBJLoader().setPath('res/models/');
-    objLoader.setMaterials(materials);
+    const mLoader = new OBJLoader().setPath('res/models/sm/');
+    mLoader.setMaterials(materials);
 
-    object = await objLoader.loadAsync('computer_backpack.obj');
-
-    object.position.y = -0.5;
-    object.rotation.y = Math.PI;
-    scene.add(object);
+    model = await mLoader.loadAsync('backpack_computer.obj');
+    
+    model.position.y = -3.25;
+    model.position.z = -1;
+    model.rotation.y = Math.PI;
+    scene.add(model);
 
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setPixelRatio(window.devicePixelRatio);
@@ -82,8 +97,11 @@ async function init() {
 
     controls = new OrbitControls(camera, renderer.domElement);
     controls.enableDamping = true;
-    controls.minDistance = 0.25;
-    controls.maxDistance = 2;
+    controls.minDistance = 3;
+    controls.maxDistance = 8;
+
+    /*const axesHelper = new THREE.AxesHelper(5);
+    scene.add(axesHelper);*/
 
     window.addEventListener('resize', onWindowResize);
 }
@@ -108,53 +126,45 @@ let loopCounter = 0;
 let text = "";
 
 let data = {
-    altitude: 1254,
-    speed: 84,
-    blink: false,
+    index: 0,
 }
 
 function animate() {
     controls.update();
 
-    object.rotation.y = Math.PI + Math.sin(loopCounter * 0.005) * 0.2;
-    object.rotation.x = Math.cos(loopCounter * 0.005) * 0.1;
+    /*let r = Math.sin(loopCounter * 0.001) * 0.2;
 
-    // Subtle screen motion
-    let a = 0.01;
-    let z = Math.sin(loopCounter * 0.25)*a + a
-    let r = Math.cos(loopCounter * 0.1)*a + a
-    //ct.repeat.set(1+z, 1+z);
-    //ct.rotation = r;
-    
+    camera.position.x = Math.sin(r) * 5;
+    camera.position.z = Math.cos(r) * 5;
+    camera.position.y = Math.sin(loopCounter * 0.01) * 0.1;*/
+
     // Update text
-    if (loopCounter % 20 == 0) {
-        // Update data
-        data.blink = !data.blink;
-        data.altitude += randInt(-2, 2);
-        data.altitude = clamp(data.altitude, 1200, 1300)
-        data.speed += randInt(-1, 1);
-        data.speed = clamp(data.speed, 64, 98)
+    if (loopCounter % 16 == 0) {
+        data.index = (data.index + 1) %  4;
+        let chars = "|/-\\";
 
-        // Generate interface
+        // Generate interface (16x8 characters)
         text = `\
-$ stat${data.blink ? "_" : ""}
+ --- B.P.C ---
+$ help
+Version: 1.0
+8-bit edition
 
-HSFV stat:
-----------
-ENG: OK
-ALT: ${data.altitude}m
-SPD: ${data.speed}kph
-----------
+
+Loading...
+${chars[data.index]}
 `
         ctx.clearRect(0, 0, c.width, c.height);
+        ctx.drawImage(image, 0, 0);
+        //ctx.drawImage(mask, 0, 0);
 
-        ctx.font = `32px minecraft`;
-        ctx.fillStyle = "#00ff00";
+        ctx.font = `16px monospace`;
+        ctx.fillStyle = "#5dd85a";
         
         let lines = text.split("\n");
         
         for (let i in lines) {
-            ctx.fillText(lines[i], margin, margin+32*i);
+            ctx.fillText(lines[i], 694, 348 + 16*i + 16);
         }
 
         ct.needsUpdate = true;
